ac3329bff0c2f363a0f2b92a4f257514461fd9be
Lang Lovdog Brandon Arthemis Nereo Inu Ookami Marquez Salazar <brandon.mozart@gmail.com>
2024-11-11 01:31:58 -0600

    Added Jqedit for live editing and query

--- plugin/jq/jq.lua
+++ plugin/jq/jq.lua
@@ -13,7 +13,18 @@ function buf_text(bufnr)
   return text
 end
 
-function set_buf_text(text, bufnr)
+function set_buf_text(text, bufnr,editfile,filter)
+  if text == nil then
+    return
+  end
+
+  if text:match('^jq:') and editfile then
+    return
+  end
+  if text:match('^jq:') then
+    text = text .. '\n in filter ' .. filter
+  end
+  
   if bufnr == nil then
     bufnr = vim.fn.bufnr('%')
   end
@@ -45,44 +56,171 @@ function jq_filter(json_bufnr, jq_filter)
   end
 end
 
-function Jq_command(horizontal)
+function Jq_command(horizontal,editfile)
   local splitcmd = 'vnew'
+  local splitcmd2 = 'vnew'
   if horizontal == true then
-    splitcmd = 'new'
+    splitcmd = 'belowright new jq-filter'
+    if editfile == true then
+      splitcmd2 = splitcmd2 .. 'vnew json-edit'
+      --reduce to 1/4 of the height
+      splitcmd = splitcmd .. ' | resize 13'
+    end
   end
 
   local json_bufnr = vim.fn.bufnr()
 
   vim.cmd(splitcmd)
   vim.cmd'set filetype=conf'
-  set_buf_text('# JQ filter: press <CR> to execute it\n\n.')
+  set_buf_text('# JQ filter: press <CR> to execute it (different queries should be separated by a blank line)\n\n.')
   vim.cmd'normal!G'
   local jq_bufnr = vim.fn.bufnr()
   local jq_winnr = vim.fn.bufwinid(jq_bufnr)
 
-  vim.cmd(splitcmd)
+  vim.cmd(splitcmd2)
   vim.cmd'set filetype=json'
   local result_bufnr = vim.fn.bufnr()
 
 
   vim.fn.win_gotoid(jq_winnr)
-
-  -- setup keybinding autocmd in the filter buffer:
-  kmap(
-    'n',
-    '<CR>',
-    function()
-      local filter = buf_text(jq_bufnr)
-      set_buf_text(jq_filter(json_bufnr, filter), result_bufnr)
+  
+  -- setup keybindig for bufdelete query buffer
+  if vim.fn.maparg('<leader>bd', 'n') ~= '' then
+    vim.keymap.del('n', '<leader>bd', { buffer = jq_bufnr })
+  end
+  kmap( 'n', '<leader>bd', function()
+      vim.cmd('bd!')
     end,
     { buffer = jq_bufnr }
   )
+
+  -- setup keybindig for saving edited json
+  if editfile == true then
+    -- setup keybinding for writing edited json
+    if vim.fn.maparg('<C-s>', 'n') ~= '' then
+      vim.keymap.del('n', '<C-s>', { buffer = jq_bufnr })
+    end
+    kmap( 'n', '<C-s>', function()
+        -- go to json buffer and save
+        vim.fn.win_gotoid(vim.fn.bufwinid(json_bufnr))
+        vim.cmd'write'
+        -- go back to jq buffer
+        vim.fn.win_gotoid(jq_winnr)
+      end,
+      { buffer = jq_bufnr }
+    )
+    --setup keybinding for searching through jsonfly
+    if vim.fn.maparg('<C-a>', 'n') ~= '' then
+      vim.keymap.del('n', '<C-a>', { buffer = jq_bufnr })
+    end
+    kmap( 'n', '<C-a>', function()
+        -- go to json buffer and save
+        vim.fn.win_gotoid(vim.fn.bufwinid(json_bufnr))
+        vim.cmd('Telescope jsonfly')
+        -- go back to jq buffer
+        --vim.fn.win_gotoid(jq_winnr)
+    end,
+    { buffer = jq_bufnr }
+    )
+    -- setup keybinding for undo
+    if vim.fn.maparg('<leader>uu', 'n') ~= '' then
+      vim.keymap.del('n', '<leader>uu', { buffer = jq_bufnr })
+    end
+    kmap( 'n', '<leader>uu', function()
+        -- go to json buffer and save
+        vim.fn.win_gotoid(vim.fn.bufwinid(json_bufnr))
+        vim.cmd('undo')
+        -- go back to jq buffer
+        vim.fn.win_gotoid(jq_winnr)
+    end,
+    { buffer = jq_bufnr }
+    )
+    -- setup keybinding for redo
+    if vim.fn.maparg('<leader>rr', 'n') ~= '' then
+      vim.keymap.del('n', '<leader>rr', { buffer = jq_bufnr })
+    end
+    kmap( 'n', '<leader>rr', function()
+        -- go to json buffer and save
+        vim.fn.win_gotoid(vim.fn.bufwinid(json_bufnr))
+        vim.cmd('redo')
+        -- go back to jq buffer
+        vim.fn.win_gotoid(jq_winnr)
+    end,
+    { buffer = jq_bufnr }
+    )
+    -- setup keybinding autocmd in the filter buffer:
+    -- editing json
+    if vim.fn.maparg('<CR>', 'n') ~= '' then
+      vim.keymap.del('n', '<CR>', { buffer = jq_bufnr })
+    end
+    kmap( 'n', '<CR>', function()
+        local filter = buf_text(jq_bufnr)
+        -- Get filters separated by double newlines
+        local filters = vim.fn.split(filter, '\n\n')
+        for i, the_filter in ipairs(filters) do
+          if the_filter:match('^#') then goto continue end
+          if the_filter == '' then goto continue end -- skip empty lines in the filter buffer
+          if the_filter == nil then goto continue end -- skip empty lines in the filter buffer
+          set_buf_text(jq_filter(json_bufnr, the_filter), json_bufnr,true,the_filter)
+          ::continue::
+        end
+        set_buf_text('# JQ filter: press <CR> to execute it\n\n', jq_bufnr,false,nil)
+      end,
+      { buffer = jq_bufnr }
+    )
+    -- setup keybinding autocmd in the filter buffer:
+    -- query json
+    if vim.fn.maparg('<leader><CR>', 'n') ~= '' then
+      vim.keymap.del('n', '<leader><CR>', { buffer = jq_bufnr })
+    end
+    kmap( 'n', '<leader><CR>', function()
+        local filter = buf_text(jq_bufnr)
+        -- Get filters separated by double newlines
+        local filters = vim.fn.split(filter, '\n\n')
+        for i, the_filter in ipairs(filters) do
+          if the_filter:match('^#') then goto continue end
+          if the_filter == '' then goto continue end -- skip empty lines in the filter buffer
+          if the_filter == nil then goto continue end -- skip empty lines in the filter buffer
+          set_buf_text(jq_filter(json_bufnr, the_filter), result_bufnr,false,the_filter)
+          ::continue::
+        end
+        set_buf_text('# JQ filter: press <CR> to execute it\n\n', jq_bufnr,false,nil)
+      end,
+      { buffer = jq_bufnr }
+    )
+
+  else
+    -- setup keybinding autocmd in the filter buffer:
+    if vim.fn.maparg('<CR>', 'n') ~= '' then
+      vim.keymap.del('n', '<CR>', { buffer = jq_bufnr })
+    end
+    kmap( 'n', '<CR>', function()
+        local filter = buf_text(jq_bufnr)
+        -- Get filters separated by double newlines
+        local filters = vim.fn.split(filter, '\n\n')
+        for i, the_filter in ipairs(filters) do
+          if the_filter:match('^#') then goto continue end
+          if the_filter == '' then goto continue end -- skip empty lines in the filter buffer
+          if the_filter == nil then goto continue end -- skip empty lines in the filter buffer
+          set_buf_text(jq_filter(json_bufnr, the_filter), result_bufnr,editfile,the_filter)
+          ::continue::
+        end
+        set_buf_text('# JQ filter: press <CR> to execute it\n\n', jq_bufnr,false,nil)
+      end,
+      { buffer = jq_bufnr }
+    )
+  end
 end
 
 ucmd('Jq', function()
-  Jq_command(false)
+  Jq_command(false,false)
+end, {})
+
+ucmd('Jqedit', function()
+  vim.cmd'set nofoldenable'
+  Jq_command(true,true)
 end, {})
 
 ucmd('Jqhorizontal', function()
-  Jq_command(true)
+  Jq_command(true,false)
 end, {})
